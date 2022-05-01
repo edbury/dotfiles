9c83ffce5cec9b150ab8614702d7c830badae428
Edbury Enegren <edbury.enegren@gmail.com>
2022-04-30 19:17:56 -0700

    fix antibody script

--- run_once_install-antibody.sh
+++ run_once_install-antibody.sh
@@ -2,7 +2,7 @@
 if  command -v antibody >/dev/null 2>&1; then
 	echo "antibody already installed"
 else
-curl -sL https://git.io/antibody | sudo sh -s -- -b /usr/local/bin
+curl -sfL git.io/antibody | sh -s - -b /usr/local/bin
 fi
 
 antibody bundle < ~/.zsh_plugins.txt > ~/.zsh_plugins.sh
