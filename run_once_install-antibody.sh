94a43c071fe5158ed447589d784406f2d0effec3
Edbury Enegren <edbury.enegren@gmail.com>
2022-04-30 19:33:59 -0700

    use template

--- run_once_install-antibody.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-if  command -v antibody >/dev/null 2>&1; then
-	echo "antibody already installed"
-else
-curl -sfL git.io/antibody | sh -s - -b /usr/local/bin
-fi
-
-antibody bundle < ~/.zsh_plugins.txt > ~/.zsh_plugins.sh
